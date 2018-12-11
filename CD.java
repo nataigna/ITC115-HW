8d1dc26219acfbb0b2d8ceb3b53a63577b4bf467
nataigna <natacarlton@gmail.com>
2018-12-10 18:54:07 -0800

    Add files via upload

---
+++ CD.java
@@ -0,0 +1,9 @@
+
+public class CD extends LibraryItem {
+
+	public CD(int number, String title, String reserve, String checkin, String checkout, double payfines) {
+		super(number, title, reserve, checkin, checkout, payfines);
+		// TODO Auto-generated constructor stub
+	}
+
+}
