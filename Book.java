90077fb38892af25d6de15305516e8e07c56b53d
nataigna <natacarlton@gmail.com>
2018-12-10 18:53:45 -0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,11 @@
+
+public class Book extends LibraryItem {
+
+	public Book(int number, String title, String reserve, String checkin, String checkout, double payfines) {
+		super(number, title, reserve, checkin, checkout, payfines);
+		
+		
+		
+	}
+
+}
