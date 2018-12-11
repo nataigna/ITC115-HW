a66266171ce45230a0d97487cd9a6a0113c4f7ce
nataigna <natacarlton@gmail.com>
2018-12-10 18:54:27 -0800

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,9 @@
+
+public class DVD extends LibraryItem {
+
+	public DVD(int number, String title, String reserve, String checkin, String checkout, double payfines) {
+		super(number, title, reserve, checkin, checkout, payfines);
+		// TODO Auto-generated constructor stub
+	}
+
+}
