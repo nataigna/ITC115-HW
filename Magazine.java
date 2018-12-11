957ce3b32a26c32424f11e958657eaead6ee8bc2
nataigna <natacarlton@gmail.com>
2018-12-10 18:54:57 -0800

    Add files via upload

---
+++ Magazine.java
@@ -0,0 +1,9 @@
+
+public class Magazine extends LibraryItem {
+
+	public Magazine(int number, String title, String reserve, String checkin, String checkout, double payfines) {
+		super(number, title, reserve, checkin, checkout, payfines);
+		// TODO Auto-generated constructor stub
+	}
+
+}
